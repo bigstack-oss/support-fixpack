8b1c0613d6f07c37cb9d78a67a807777c3435d19
Roy Tan <yooniks9@gmail.com>
2026-07-02 12:36:51 +0800

    split code

--- install.sh
+++ install.sh
@@ -25,44 +25,7 @@ else
     echo "[INFO] Update admin quota to unlimited"
     openstack quota set --cores -1 --ram -1 --instances -1 --volumes -1 --gigabytes -1 --key-pairs -1 admin
 
-    # 4. update flavors
-    echo "[INFO] Update OPSTK flavor"
-    openstack flavor delete pgpu.example
-    openstack flavor delete vgpu.example
-    openstack flavor delete t2.micr
-    openstack flavor delete t2.pico
-    openstack flavor delete t2.nano
-    openstack flavor create --vcpus 8 --ram 8192 --disk 200 --property hw:cpu_cores=8 --public appfw.medium
-    openstack flavor create --vcpus 12 --ram 12288 --disk 200 --property hw:cpu_cores=12 --public appfw.large
-    openstack flavor create --vcpus 16 --ram 16384 --disk 200 --property hw:cpu_threads=2 --property hw:cpu_cores=8 --public appfw.xlarge
-    openstack flavor create --vcpus 1 --ram 1024 --disk 60 --public a1.micro
-    openstack flavor create --vcpus 2 --ram 2048 --disk 60 --public a1.small
-    openstack flavor create --vcpus 4 --ram 4096 --disk 60 --property hw:cpu_cores=4 --public a1.medium
-    openstack flavor create --vcpus 8 --ram 8192 --disk 60 --property hw:cpu_cores=8 --public a1.large
-    openstack flavor create --vcpus 16 --ram 16384 --disk 60 --property hw:cpu_threads=2 --property hw:cpu_cores=8 --public a1.xlarge
-    openstack flavor create --vcpus 32 --ram 32768 --disk 60 --property hw:cpu_threads=2 --property hw:cpu_cores=16 --public a1.2xlarge
-    openstack flavor create --vcpus 1 --ram 1024 --disk 120 --public a2.micro
-    openstack flavor create --vcpus 2 --ram 2048 --disk 120 --public a2.small
-    openstack flavor create --vcpus 4 --ram 4096 --disk 120 --property hw:cpu_cores=4 --public a2.medium
-    openstack flavor create --vcpus 8 --ram 8192 --disk 120 --property hw:cpu_cores=8 --public a2.large
-    openstack flavor create --vcpus 16 --ram 16384 --disk 120 --property hw:cpu_threads=2 --property hw:cpu_cores=8 --public a2.xlarge
-    openstack flavor create --vcpus 32 --ram 32768 --disk 120 --property hw:cpu_threads=2 --property hw:cpu_cores=16 --public a2.2xlarge
-    openstack flavor create --vcpus 4 --ram 4096 --disk 10 --property hw:cpu_cores=4 --public b10.small
-    openstack flavor create --vcpus 8 --ram 8192 --disk 10 --property hw:cpu_cores=8 --public b10.medium
-    openstack flavor create --vcpus 4 --ram 4096 --disk 20 --property hw:cpu_cores=4 --public b20.small
-    openstack flavor create --vcpus 8 --ram 8192 --disk 20 --property hw:cpu_cores=8 --public b20.medium
-    openstack flavor create --vcpus 4 --ram 4096 --disk 40 --property hw:cpu_cores=4 --public b40.small
-    openstack flavor create --vcpus 8 --ram 8192 --disk 40 --property hw:cpu_cores=8 --public b40.medium
-    openstack flavor create --vcpus 4 --ram 4096 --disk 80 --property hw:cpu_cores=4 --public b80.small
-    openstack flavor create --vcpus 8 --ram 8192 --disk 80 --property hw:cpu_cores=8 --public b80.medium
-    openstack flavor create --vcpus 4 --ram 8192 --disk 80 --property hw:cpu_cores=8 --public k8s.small
-    openstack flavor create --vcpus 8 --ram 16384 --disk 80 --property hw:cpu_threads=2 --property hw:cpu_cores=4 --public k8s.medium
-    openstack flavor create --vcpus 16 --ram 32768 --disk 80 --property hw:cpu_threads=2 --property hw:cpu_cores=8 --public k8s.large
-    openstack flavor create --vcpus 2 --ram 2048 --disk 80 --property hw:cpu_cores=2 --public basic.small
-    openstack flavor create --vcpus 4 --ram 4096 --disk 80 --property hw:cpu_cores=4 --public basic.medium
-    openstack flavor create --vcpus 8 --ram 8192 --disk 80 --property hw:cpu_cores=8 --public basic.large
-    openstack flavor create --vcpus 16 --ram 16384 --disk 80 --property hw:cpu_threads=2 --property hw:cpu_cores=8 --public basic.xlarge
-    openstack flavor create --vcpus 32 --ram 32768 --disk 80 --property hw:cpu_threads=2 --property hw:cpu_cores=16 --public basic.2xlarge
+
 
     # 5. Override number of enabled pcie ports & enable swtpm for libvirt
     echo "[INFO] Override number of enabled pcie ports & enable swtpm for libvirt"
