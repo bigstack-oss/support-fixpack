f6a7a254645ac078f4cd484d6423946220fda49e
Roy Tan <yooniks9@gmail.com>
2026-07-07 10:56:13 +0800

    update install scripts

--- install.sh
+++ install.sh
@@ -18,41 +18,45 @@ else
     # 2. 複製 ext-* 工具
     echo "[INFO] Copying ext-* tools to /usr/local/bin..."
     cp ./ext-* /usr/local/bin/
-    git add /usr/local/bin/ext-*
+    git add /usr/local/bin/ext-change-port-ip
+    git add /usr/local/bin/ext-change-route-ip
+    git add /usr/local/bin/ext-change-vm-pass
+    git add /usr/local/bin/ext-image-config
+    git add /usr/local/bin/ext-snapshot-restore
+    git add /usr/local/bin/ext-volume-config
+    git add /usr/local/bin/ext-volume-migrate
     hex_sdk git_push "Add support-ext-pack"
 
     # 3. update admin quota unlimit
     echo "[INFO] Update admin quota to unlimited"
     openstack quota set --cores -1 --ram -1 --instances -1 --volumes -1 --gigabytes -1 --key-pairs -1 admin
 
-
-
-    # 5. Override number of enabled pcie ports & enable swtpm for libvirt
+    # 4. Override number of enabled pcie ports & enable swtpm for libvirt
     echo "[INFO] Override number of enabled pcie ports & enable swtpm for libvirt"
     mkdir -p /etc/nova/nova.d/
     cp ./override/custom-nova.conf /etc/nova/nova.d/custom.conf
     cubectl node -r compute rsync /etc/nova/nova.d/custom.conf
     cubectl node -r compute exec -p "hex_config restart_nova"
 
-    # 6. update rabbitmq configuration
+    # 5. update rabbitmq configuration
     mkdir -p /etc/systemd/system/rabbitmq-server.service.d
     cp ./override/custom-rabbitmq.conf /etc/systemd/system/rabbitmq-server.service.d/custom.conf
     cubectl node -r control rsync /etc/systemd/system/rabbitmq-server.service.d/custom.conf
     cubectl node -r control exec -p "systemctl daemon-reload"
     cubectl node -r control exec -p "systemctl restart rabbitmq-server"
 
-    # 7. update rabbitmq configuration
+    # 6. update rabbitmq configuration
     sed -i 's/os_type=windows/os_type=windows --property hw_disk_bus=virtio/g' /usr/lib/hex_sdk/modules/sdk_os.sh
     git add /usr/lib/hex_sdk/modules/sdk_os.sh
     hex_sdk git_push "Fix missing virtio disk bus property for windows image"
 
-    # 8. Create marker file to indicate the installation is done
+    # 7. Create marker file to indicate the installation is done
     echo "[INFO] Marking installation as done..." > /etc/appliance/state/install_support_ext_pack_done
     cubectl node -r control rsync /etc/appliance/state/install_support_ext_pack_done
 
-    # 9. add rke2 generator
-    cp ./rke2_generator /usr/local/bin/
-    git add /usr/local/bin/rke2_generator
+    # 8. add rke2 generator
+    cp ./rke2-generator /usr/local/bin/
+    git add /usr/local/bin/rke2-generator
     hex_sdk git_push "add rke2 generator"
 fi
 
